a469470effa8e691a55da27f04eb5f8ee243c184
Mateo <madiazt@unal.edu.co>
2021-08-13 23:39:26 -0500

    added log in

--- controllers/log-in.js
+++ controllers/log-in.js
@@ -3,11 +3,14 @@ const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 
 exports.login_get = (req, res, next) => {
-    // code here
+    res.render("log-in", {
+        title:"log in"
+    })
 };
 
 exports.login_post = (req, res, next) => {
     // code here
+    res.send("todo")
 };
 
 exports.logout_get = (req, res, next) => {

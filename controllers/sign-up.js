416a0176c80a88fa2eaa539314503639233ab4be
Mateo <madiazt@unal.edu.co>
2021-08-13 23:02:26 -0500

    added sign-up form functional skeleton

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -1,14 +1,77 @@
 const async = require("async");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.create_get = (req, res, next) => {
     res.render("sign-up", {
-        title:"sign up"
+        title:"sign up",
+        errors: undefined
     })
 };
 
-exports.create_post = (req, res, next) => {
-    // code here
-    res.send("to do")
-};
+exports.create_post = [
+    //validate and sanitize sign up foorm data
+    body("username", "an Email is required.").trim().isLength({ min:1 }).escape(),
+    body("first_name", "first name is required").trim().isLength({ min:1 }).escape(),
+    body("second_name", "second name is required").trim().isLength({ min: 1}).escape(),
+    body("password_1", "a password is required").trim().isLength({ min: 1}).escape(),
+    body("password_2").custom((value, { req} ) => {
+        // check password confirmation
+        if (value !== req.body.password_1) {
+            throw new Error("passwords do not match!");
+        };
+
+        //succes of the validator
+        return true;
+    }).escape(),
+
+    // Procces request after sanitization of form data
+    (req, res, next) => {
+
+        // Check errors is form data
+        const errors = validationResult(req);
+        console.log(errors)
+        if (!errors.isEmpty()) {
+            //THere are errors. Render the form again!
+            res.render("sign-up", {
+                title: "sign up",
+                errors: errors.array()
+            })
+            return;
+        }
+
+        //todo: crypt password and password type in input type on view
+        const password = req.body.password_1;
+        // Create user object with trimmed data
+        const user = new User({
+            username: req.body.username,
+            password, password,
+            firstName: req.body.first_name,
+            secondName: req.body.second_name,
+        });
+
+        // Check if a user with the same username already exist!
+        User.findOne({ "username": req.body.username })
+            .exec( function(err, found_user) {
+                if (err) { return next(err) }
+
+                if (found_user) {
+                    foundUser_error = [{
+                        msg:"A user already exist with this e-mail.",
+                    }];
+                    res.render("sign-up", {
+                        title: "sign up",
+                        errors: foundUser_error,
+                    })
+                    return
+                }
+
+                // a user does not exist with the entered email, continue
+                user.save( function (err) {
+                    if (err) { return next(err) }
+                    res.redirect("/")
+                })
+            }) 
+    }
+]

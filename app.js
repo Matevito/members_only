2c21ae1fbd5d41ad2e5cc673aa730d970078817c
Mateo <madiazt@unal.edu.co>
2021-08-13 23:09:19 -0500

    added skeleton of localstrategy in app.js

--- app.js
+++ app.js
@@ -14,6 +14,33 @@ const indexRouter = require("./routes/index");
 const messagesRouter = require("./routes/messages");
 const userRouter = require("./routes/user");
 
+// setting up localstrategy
+const User = require("./models/user");
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, { message: "Incorrect username" });
+        }
+        if (user.password !== password) {
+            return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+        });
+    })
+);
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    });
+});
+
 // change process.env.DB_DATAB for your mongodb private route
 require("dotenv").config()
 mongoose.connect(process.env.DB_DATAB, {
